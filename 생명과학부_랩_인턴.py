b26c3ed95c34cd4ac237acc089b8659dddb478c3
MariGold <91964419+zmtmqhdl@users.noreply.github.com>
2024-10-01 15:12:18 +0900

    Create 생명과학부_랩_인턴.py

---
+++ 생명과학부_랩_인턴.py
@@ -0,0 +1,46 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+N, M, K = map(int, input().split())
+molds = []
+for _ in range(K) :
+    x, y, s, d, b = map(int, input().split())
+    molds.append((x - 1, y - 1, s, d - 1, b))
+ans = 0
+
+for y in range(M) :
+    molds.sort(key=lambda x : (x[1], x[0]))
+    for i in range(len(molds)) :
+        if molds[i][1] == y :
+            ans += molds[i][4]
+            molds.pop(i)
+            break
+    
+    board = [[[] for _ in range(M)] for _ in range(N)]
+    for mold in molds :
+        x, y, s, d, b = mold
+        for _ in range(s) :
+            nx, ny = x + dx[d], y + dy[d]
+            if not (0 <= nx < N and 0 <= ny < M) :
+                if d == 0 :
+                    d = 1
+                elif d == 1 :
+                    d = 0
+                elif d == 2 :
+                    d = 3
+                elif d == 3 :
+                    d = 2
+                nx, ny = x + dx[d], y + dy[d]
+            x, y = nx, ny
+        board[x][y].append((s, d, b))
+    
+    molds = []
+    for i in range(N) :
+        for j in range(M) :
+            if len(board[i][j]) > 1 :
+                largest_mold = max(board[i][j], key=lambda x : x[2])
+                molds.append((i, j, largest_mold[0], largest_mold[1], largest_mold[2]))
+            elif len(board[i][j]) == 1 :
+                molds.append((i, j, board[i][j][0][0], board[i][j][0][1], board[i][j][0][2]))
+
+print(ans)
